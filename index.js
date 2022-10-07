84c6c711050868955e010630120945e259dd4f58
Normi <normixbox@gmail.com>
2022-10-07 19:14:33 +0200

    a

--- index.js
+++ index.js
@@ -130,9 +130,7 @@ async function checkNetworth(name) {
     }
     const url = `https://skyhelper-dxxxxy.herokuapp.com/v1/profiles/${name}`
     const response = await axios.get(url, config)
-    let networth = response.data['networth']
-    console.log(networth + " networth")
-
+    let networth = response.data['unsoulboundNetworth']
     if (networth / 1000000 >= 1) {
         networth = networth / 1000000
         networth = networth.toFixed(2)
